487cd28f4549fdbb2a4bb75aeb64d862ece91729
Gleb Klepko <gleb.klepko@yahoo.com>
2022-03-09 14:34:29 -0600

    Removed unused namespaces.

--- Ping My Stuff/Program.cs
+++ Ping My Stuff/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.NetworkInformation;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ping_My_Stuff
 {
